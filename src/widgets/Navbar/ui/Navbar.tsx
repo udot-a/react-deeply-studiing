b6a9278091f84ddd6ae62d78accb24627383bd89
andrii <andrii.udot@vilmate.com>
2023-10-28 21:50:05 +0300

    Add i18next plugin

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -2,20 +2,22 @@ import React, { FC } from 'react';
 import { classNames } from 'shared/lib/classNames/classNames';
 import cls from './Navbar.module.scss';
 import { AppLink, AppLinkTheme } from 'shared/ui/AppLink/AppLink';
+import { useTranslation } from 'react-i18next';
 
 interface NavbarProps {
   className?: string;
 }
 
 export const Navbar: FC<NavbarProps> = ({ className }) => {
+	const { t } = useTranslation('translation');
 	return (
 		<div className={classNames(cls.navbar, {}, [className])}>
 			<div className={cls.links}>
 				<AppLink to={'/'} theme={AppLinkTheme.SECONDARY}>
-          Go to the main page
+					{t('goToMain')}
 				</AppLink>
 				<AppLink to={'/about'} theme={AppLinkTheme.SECONDARY}>
-          Go to the about page
+					{t('goToAbout')}
 				</AppLink>
 			</div>
 		</div>
