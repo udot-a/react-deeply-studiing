052497ec7543302eb666063731847dfd62d27750
andrii <andrii.udot@vilmate.com>
2023-11-26 17:10:37 +0200

    Add ui tests;

---
+++ config/storybook/preview.ts
@@ -0,0 +1,18 @@
+import { addDecorator } from '@storybook/react';
+import { ThemeDecorator } from '../../src/shared/config/storybook/ThemeDecorator/ThemeDecorator';
+import { Theme } from '../../src/app/providers/ThemeProvider';
+import '../../src/app/styles/index.scss';
+// import { StyleDecorator } from '../../src/shared/config/storybook/StyleDecorator/StyleDecorator';
+
+export const parameters = {
+	actions: { argTypesRegex: '^on[A-Z].*' },
+	controls: {
+		matchers: {
+			color: /(background|color)$/i,
+			date: /Date$/,
+		},
+	},
+};
+
+// addDecorator(StyleDecorator);
+// addDecorator(ThemeDecorator(Theme.DARK));
