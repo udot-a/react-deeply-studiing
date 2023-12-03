5604ea6ea77baca858e71df2f00bd2e253322228
andrii <andrii.udot@vilmate.com>
2023-12-03 20:25:12 +0200

    Added renderComponent;

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.stories.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.stories.tsx
@@ -4,6 +4,7 @@ import { Sidebar } from './Sidebar';
 import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator/ThemeDecorator';
 import { Theme } from 'app/providers/ThemeProvider';
 import { StyleDecorator } from 'shared/config/storybook/StyleDecorator/StyleDecorator';
+import { RouterDecorator } from 'shared/config/storybook/RouterDecorator/RouterDecorator';
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
@@ -21,9 +22,9 @@ const Template: ComponentStory<typeof Sidebar> = (args) => <Sidebar {...args} />
 export const Light = Template.bind({});
 // More on args: https://storybook.js.org/docs/react/writing-stories/args
 Light.args = {};
-Light.decorators = [StyleDecorator, ThemeDecorator(Theme.DARK)];
+Light.decorators = [RouterDecorator, StyleDecorator, ThemeDecorator(Theme.DARK)];
 
 export const Dark = Template.bind({});
 // More on args: https://storybook.js.org/docs/react/writing-stories/args
 Dark.args = {};
-Dark.decorators = [StyleDecorator, ThemeDecorator(Theme.LIGHT)];
+Dark.decorators = [RouterDecorator, StyleDecorator, ThemeDecorator(Theme.LIGHT)];
--- src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
@@ -1,15 +1,15 @@
 import { fireEvent, screen } from '@testing-library/react';
 import { Sidebar } from 'widgets/Sidebar';
-import { renderWithTranslation } from 'shared/lib/tests/renderWithTranslation/renderWithTranslation';
+import { renderComponent } from 'shared/lib/tests/componentRender/componentRender';
 
 describe('Sidebar Widget', () => {
 	test('Is sidebar render', () => {
-		renderWithTranslation(<Sidebar />);
+		renderComponent(<Sidebar />);
 		expect(screen.getByTestId('sidebar')).toBeInTheDocument();
 	});
 
 	test('Is toggle button change sidebar class', () => {
-		renderWithTranslation(<Sidebar />);
+		renderComponent(<Sidebar />);
 		const toggleBtn = screen.getByTestId('sidebar-toggle');
 		fireEvent.click(toggleBtn);
 		expect(screen.getByTestId('sidebar')).toHaveClass('collapsed');
