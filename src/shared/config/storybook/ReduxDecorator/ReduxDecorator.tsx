a3374a3263cc211a35cf6b274cb4ec9a50e98448
andrii <andrii.udot@vilmate.com>
2023-12-18 10:25:30 +0200

    Added Redux Store;

--- src/shared/config/storybook/ReduxDecorator/ReduxDecorator.tsx
+++ src/shared/config/storybook/ReduxDecorator/ReduxDecorator.tsx
@@ -1,6 +1,7 @@
 import { Story } from '@storybook/react';
 import { StateSchema, StoreProvider } from 'app/providers/StoreProvider';
 
+// eslint-disable-next-line react/display-name
 export const ReduxDecorator = (initialState: StateSchema) => (StoryComponent: Story) => {
 	return (
 		<StoreProvider initialState={initialState}>
