9a52073657e823fdadee097b63f37d448e35c581
andrii <andrii.udot@vilmate.com>
2024-01-09 14:01:05 +0200

    Fixed TS errors after implementation strict mode;

--- config/storybook/webpack.config.ts
+++ config/storybook/webpack.config.ts
@@ -11,11 +11,12 @@ export default ({ config }: { config: webpack.Configuration}) => {
 		src: path.resolve(__dirname, '..', '..', 'src'),
 	};
 
-	config.resolve.modules.push(paths.src);
-	config.resolve.extensions.push('.ts', '.tsx');
-	config.module.rules.push(buildCssLoader(true));
+	config!.resolve!.modules!.push(paths.src);
+	config!.resolve!.extensions!.push('.ts', '.tsx');
+	config!.module!.rules!.push(buildCssLoader(true));
 
-	config.module.rules = config.module.rules.map((rule: RuleSetRule) => {
+	// @ts-ignore
+	config!.module!.rules = config!.module!.rules!.map((rule: RuleSetRule) => {
 		if (/svg/.test(rule.test as string)) {
 			return { ...rule, exclude: /\.svg$/i };
 		}
@@ -23,18 +24,18 @@ export default ({ config }: { config: webpack.Configuration}) => {
 		return rule;
 	});
 
-	config.module.rules.push({
+	config!.module!.rules!.push({
 		test: /\.svg$/,
 		use: ['@svgr/webpack'],
 	});
 
-	config.plugins.push(
+	config!.plugins!.push(
 		new webpack.DefinePlugin({
 			__IS_DEV__: true
 		})
 	);
 
-	config.plugins.push(
+	config!.plugins!.push(
 		new webpack.DefinePlugin({
 			__IS_DEV__: JSON.stringify(true),
 			__API__: JSON.stringify(''),
