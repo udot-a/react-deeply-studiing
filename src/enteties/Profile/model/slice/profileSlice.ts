97b4a1206c3877b4f4d77373c4f977acf5593747
andrii <andrii.udot@vilmate.com>
2024-01-28 17:28:15 +0200

    Added Skeleton;

--- src/enteties/Profile/model/slice/profileSlice.ts
+++ src/enteties/Profile/model/slice/profileSlice.ts
@@ -1,5 +1,5 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
-import { Profile, ProfileSchema, ValidateProfileError } from '../types/profile';
+import { Profile, ProfileSchema } from '../types/profile';
 import { fetchProfileData } from '../services/fetchProfileData/fetchProfileData';
 import { updateProfileData } from '../services/updateProfileData/updateProfileData';
 
