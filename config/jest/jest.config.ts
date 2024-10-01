17b56d393800c4d9cb3e77e3946ea8b007ed855a
andrii <andrii.udot@vilmate.com>
2024-10-01 12:57:49 +0300

    Added report page;

--- config/jest/jest.config.ts
+++ config/jest/jest.config.ts
@@ -85,7 +85,7 @@ export default {
 		['jest-html-reporters', {
 			'publicPath': '<rootDir>/reports/unit',
 			'filename': 'report.html',
-			'openReport': false,
+			'openReport': true,
 			'inlineSource': true,
 		}]
 	],
